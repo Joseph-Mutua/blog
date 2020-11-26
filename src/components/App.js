816789577df9707224d245fd3a530318696534d5
Joseph Mutua <Mutuaj793@gmail.com>
2020-11-26 12:15:36 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const App = () => {
+    return ( 
+        <div className="ui container">App</div>
+     );
+}
+ 
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default PostList;
