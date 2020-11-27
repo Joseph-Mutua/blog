d1866fc4e63e5d1f57d6b459060a3cafcaf3cedb
Joseph Mutua <Mutuaj793@gmail.com>
2020-11-27 10:23:40 +0300

    List biuilding

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,18 +3,33 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
 class PostList extends Component {
-componentDidMount() {
+  componentDidMount() {
     this.props.fetchPosts();
-}
+  }
 
-  state = {};
-  render() {
-    return <div>Post List</div>;
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+ render() {
+  return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
-  return {posts: state.props}
-}
+  return { posts: state.props };
+};
 
 export default connect(mapStateToProps, { fetchPosts })(PostList);
